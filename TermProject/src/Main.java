bf9f3ec2d86eae67e6b45db94094b9e6f73c67e8
seoulkkk <70684827+seoulkkk@users.noreply.github.com>
2020-11-27 21:31:52 +0900

    Add files via upload

--- TermProject/src/Main.java
+++ TermProject/src/Main.java
@@ -7,6 +7,10 @@ public class Main {
 		
 		Scanner scan = new Scanner(System.in);
 		
+		//time@@@@@@@@@@
+		long t = System.currentTimeMillis();
+		
+		//배열크기@@@@@@@@@@nNNNNNNNNNN
 		System.out.print("생성할 배열의 크기를 입력하세요");
 		int N = scan.nextInt();
 		
@@ -19,34 +23,54 @@ public class Main {
 		}
 		System.out.println(Arrays.toString(array));
 		
-		System.out.print("구간a:");
-		int a = scan.nextInt();
-		System.out.print("구간b:");
-		int b = scan.nextInt();
+		//K입력받기@@@@@@@@@
+		System.out.print("범위 갯수 입력:");
+		int K = scan.nextInt();
 		scan.close();
+		int j;
 		
-		int max = array[a];
-		int min = array[a];
-				
-		for(int i=a-1;i<b-1;i++) {
-		    if(max<array[i]) {
-		    	max = array[i];
-		    }
-					
-		    if(min>array[i]) {
-		    	min = array[i];
-		    }
+		for(j=1; j<=K; j++) {
+		//구간@@@@@@@@@@@@
+
+			int a = (int)(Math.random()*N)+1;
+
+			int b = (int)(Math.random()*N)+1;
+			int c = 0;
+			
+			//작은값큰값변경
+			if(a>b) {
+				c = a;
+				a = b;
+				b = c;
+			}
+			
+			System.out.println("구간"+a+"부터"+b+"까지");
+			
+			int max = array[a];
+			int min = array[a];
+
+			for(int i=a-1;i<b-1;i++) {
+			    if(max<array[i]) {
+			    	max = array[i];
+			    }
+
+			    if(min>array[i]) {
+			    	min = array[i];
+			    }
+			}
+
+			System.out.println("최대값 : "+max);
+			System.out.println("최소값 : "+min);
+
+
+			int sum = 0;
+			for(int i = a-1; i <b; i++) {
+				sum += array[i];
+			}
+			System.out.println("합계:" + sum);
+			
 		}
-				
-		System.out.println("최대값 : "+max);
-		System.out.println("최소값 : "+min);
-		
-		
-		int sum = 0;
-		for(int i = a-1; i <b; i++) {
-			sum += array[i];
-		}
-		System.out.println("합계:" + sum);
+		System.out.println("Elapsed time is "+(System.currentTimeMillis()-t+" ms"));
 	}
 
 }
